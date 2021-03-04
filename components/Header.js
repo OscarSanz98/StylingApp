a6a9f1c67d9fa0e57e863a022fd6d3f9dfa829c0
Oscar Sanz <oscar.sanz_97@hotmail.com>
2021-03-04 18:15:01 +0000

    Implement Platform API
    Used Platform API to apply styling depending on what platform is used

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import Colors from '../constants/colors';
 
 const Header = props => {
     return (
-        <View style={styles.header}>
+        <View style={{...styles.headerBase, ...Platform.select({ios: styles.headeriOS, android: styles.headerAndroid})}}>
             <Text style={styles.headerTitle}>{props.title}</Text>
         </View>
     );
@@ -13,19 +13,33 @@ const Header = props => {
 
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase : {
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: Colors.primary,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        
+
+    },
+
+    headeriOS: {
+        backgroundColor: 'white',
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+    },
+
+    headerAndroid: {
+        backgroundColor: Colors.primary,
+        borderBottomColor: 'transparent',
+        borderBottomWidth:  0,
     },
 
     headerTitle: {
-        color: 'black',
+        color: Platform.OS === "ios" ? Colors.primary : 'white',
         fontSize: 18,
-        fontFamily: 'open-sans-bold'
+        fontFamily: 'open-sans-bold',
+        
     }
 });
 
